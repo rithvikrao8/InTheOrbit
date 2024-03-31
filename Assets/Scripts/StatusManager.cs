79abe929367a7797151b327e150bbc18b033def6
Mehuli Nath <nath.mehuli@gmail.com>
2024-03-31 00:40:54 -0700

    Add back Status Canvas

--- Assets/Scripts/StatusManager.cs
+++ Assets/Scripts/StatusManager.cs
@@ -4,7 +4,6 @@ using TMPro; // Uncomment if using TextMeshPro elements
 
 public class StatusManager : MonoBehaviour
 {
-    [SerializeField] public TextMeshProUGUI livesText;
     [SerializeField] public TextMeshProUGUI killsText;
     [SerializeField] public TextMeshProUGUI damageText;
     [SerializeField] Canvas statusCanvas;
@@ -15,43 +14,32 @@ public class StatusManager : MonoBehaviour
     
     public void Start()
     {
-        Reset(10,0,0);
+        Reset(4,6);
         Debug.Log("StatusManager Start() called");
     }
 
     public void IncreaseKills()
     {
         kills++;
+        Update();
     }
 
     public void IncreaseDamage()
     {
         damage++;
-    }
-
-    public void DecreaseLives()
-    {
-        if (lives > 1)
-        {
-            lives--;
-        }
+        Update();
     }
 
     private void Update()
     {
         killsText.text = kills.ToString();
         damageText.text = damage.ToString();
-        if (lives > 0)
-           livesText.text = lives.ToString();
-        else
-            livesText.text = " :(";
     }
 
-    public void Reset(int l, int k, int d)
+    public void Reset(int k, int d)
 {
     kills = k;
         damage = d;
-        lives = l;
         Update();
     }
 }

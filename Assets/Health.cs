ffe305bb92da8cdd9f738fdc92bb88dc84af8041
EssonZ <Junyue0612@gmail.com>
2024-05-15 00:44:56 -0700

    Added back the healthbar

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -10,7 +10,7 @@ public class Health : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,26 +19,29 @@ public class Health : MonoBehaviour
         Test();
 
 
-        
+
     }
 
     public IEnumerator takeDamage(float damage)
     {
+        int initalhealth = (int)healthAmount;
 
-        for (int i = 0; i < (damage*10); i++)
+        for (int i = 0; i < (damage * 10); i++)
         {
 
             healthAmount -= (float)0.1;
             healthbar.fillAmount = healthAmount / 100;
-            yield return new WaitForSecondsRealtime((float)0.00001);
+            yield return new WaitForSecondsRealtime((float)0.0001);
 
         }
+        healthAmount = initalhealth - ((int)damage); //this is to prevent 80.0000031
     }
 
     public IEnumerator Heal(float healing)
     {
+        int initalhealth = (int)healthAmount;
 
-        for (int i = 0; i < (healing*10); i++)
+        for (int i = 0; i < (healing * 10); i++)
         {
             healthAmount += (float)0.1;
             healthAmount = Mathf.Clamp(healthAmount, 0, 100);
@@ -46,6 +49,8 @@ public class Health : MonoBehaviour
             healthbar.fillAmount = healthAmount / 100;
             yield return new WaitForSeconds((float)0.00001);
         }
+
+        healthAmount = initalhealth + ((int)healing);
     }
 
     public IEnumerator Delay()
@@ -57,10 +62,10 @@ public class Health : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-           StartCoroutine(takeDamage(20));
+            StartCoroutine(takeDamage(20));
             Debug.Log("balls");
         }
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             StartCoroutine(Heal(20));
         }
